23ca1e60f3f0fe539060beed4ed4163b03271ee4
SaifEddine-BenTurkia <saifeddine.benturkia@supcom.tn>
2025-12-05 21:54:29 +0100

    unnecessary dirs deleted

--- build/robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pc/projects/Intelligent-mutli-Model-Robot-agent/navigation-system/robot_description
-# Build directory: /home/pc/projects/Intelligent-mutli-Model-Robot-agent/build/robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
